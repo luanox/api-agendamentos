b9ac866436b7b463443b6e084755d3634a208807
Luan Neves <luanuemg@hotmail.com>
2020-06-28 11:00:54 -0300

    create: routes users and sessions

--- src/services/CreateAppointmentService.ts
+++ src/services/CreateAppointmentService.ts
@@ -1,32 +1,32 @@
+import {startOfHour} from 'date-fns'
+import { getCustomRepository } from 'typeorm'
+
 import Appointment from '../models/Appointment'
 import AppointmentsRepository from '../repositories/AppointmentsRepository'
-import {startOfHour} from 'date-fns'
 
 interface Request {
-    provider: string
+    provider_id: string
     date: Date
 }
 
 class CreateAppointmentService {
-    private appointmentsRepository: AppointmentsRepository
+    public async execute({provider_id, date}: Request): Promise<Appointment> {
+        const appointmentsRepository = getCustomRepository(AppointmentsRepository)
 
-    constructor(appointmentsRepository: AppointmentsRepository){
-        this.appointmentsRepository = appointmentsRepository
-    }
-
-    public execute({provider, date}: Request): Appointment {
         const appointmentDate = startOfHour(date)
 
-        const findAppointmentInSameDate = this.appointmentsRepository.findByDate(appointmentDate)
+        const findAppointmentInSameDate = await appointmentsRepository.findByDate(appointmentDate)
 
         if(findAppointmentInSameDate){
             throw Error("This appointment is already booked")
         }
 
-        const appointment = this.appointmentsRepository.create({
-            provider, 
+        const appointment = appointmentsRepository.create({
+            provider_id, 
             date: appointmentDate
         })
+
+        await appointmentsRepository.save(appointment)
 
         return appointment
     }
