6910b0d95d810f1b080877589ede3bb3b9da0b83
Luan Neves <luanuemg@hotmail.com>
2020-08-24 06:02:09 -0300

    refactor: repositories appointments

--- src/modules/appointments/services/CreateAppointmentService.ts
+++ src/modules/appointments/services/CreateAppointmentService.ts
@@ -3,7 +3,7 @@ import { getCustomRepository } from 'typeorm'
 
 import AppError from '@shared/errors/AppError'
 import Appointment from '@modules/appointments/infra/typeorm/entities/Appointment'
-import AppointmentsRepository from '@modules/appointments/repositories/AppointmentsRepository'
+import AppointmentsRepository from '@modules/appointments/infra/typeorm/repositories/AppointmentsRepository'
 
 interface Request {
     provider_id: string
@@ -22,12 +22,10 @@ class CreateAppointmentService {
             throw new AppError("This appointment is already booked")
         }
 
-        const appointment = appointmentsRepository.create({
+        const appointment = await appointmentsRepository.create({
             provider_id, 
             date: appointmentDate
         })
-
-        await appointmentsRepository.save(appointment)
 
         return appointment
     }
