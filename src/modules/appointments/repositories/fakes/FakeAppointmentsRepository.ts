cfca9d387239e8104e3f1d7091956263bb42500a
Luan Neves <luanuemg@hotmail.com>
2020-08-31 06:15:11 -0300

    test: create test appointments

---
+++ src/modules/appointments/repositories/fakes/FakeAppointmentsRepository.ts
@@ -0,0 +1,31 @@
+import {uuid} from 'uuidv4'
+
+import IAppointmentsRepository from '@modules/appointments/repositories/IAppointmentsRepository'
+import Appointment from '@modules/appointments/infra/typeorm/entities/Appointment'
+import ICreateAppointmentDTO from '@modules/appointments/dtos/ICreateAppointmentDTO'
+
+class AppointmentsRepository implements IAppointmentsRepository{
+    private appointments: Appointment[] = [];
+    
+    public async findByDate(date: Date): Promise<Appointment | undefined >{
+        const findAppointment = this.appointments.find(appointment => appointment.date === date)
+
+        return findAppointment;
+    }
+
+    public async create({provider_id, date}: ICreateAppointmentDTO): Promise<Appointment> {
+       const appointment = new Appointment();
+
+       appointment.id = uuid();
+       appointment.date = date;
+       appointment.provider_id = provider_id;
+
+       this.appointments.push(appointment)
+
+       return appointment;
+    }
+
+
+}
+
+export default AppointmentsRepository
